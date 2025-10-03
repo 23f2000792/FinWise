73eb034f13fcd2ea8cec5f9808280a98ddc389bc
Krish Gupta <krishgupta200510@gmail.com>
2025-10-03 16:43:56 +0000

    Error: No Output Directory named "public" found after the Build complete

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone',
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -29,7 +30,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https' as const,
         hostname: 'lh3.googleusercontent.com',
         port: '',
         pathname: '/**',
